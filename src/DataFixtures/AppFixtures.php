7b6c6f490fdabc9ad59976725ac90c62519e61d5
janick <janick.buisson@hotmail.fr>
2021-02-23 13:03:14 +0100

    Remeber me security.yaml and login.html.twig

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,12 +5,22 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Faker;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager)
+    public function load(ObjectManager $em)
     {
+        $faker = Faker\Factory::create('fr_FR');
 
-        $manager->flush();
+        $populator = new Faker\ORM\Doctrine\Populator($faker, $em);
+        $populator->addEntity(User::class, 10);
+        $em->persist($populator);
+        $em->flush();
     }
 }
